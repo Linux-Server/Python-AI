7876c1805f54888d0f5d819b28a96108a3e98fb3
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-08-14 16:45:57 +0530

    Types

---
+++ module_1/01_types.py
@@ -0,0 +1,34 @@
+# types
+
+name= "Sachin"
+age =22
+status =True
+
+print("Name is :" ,type(name), "age type is : ", type(age), "status type is : ", type(status))
+
+
+# Mainly 5 data types in python
+# int, string, float, bool,None
+
+# Integer
+age =22 # +ve
+age_two = -100 # -ve
+age_three = 0 # zero
+
+# Strings
+name= "sam" # double quote
+name_one ='ray' # single quote
+name_two = '''Welcome onboard''' # triple quote
+
+
+# Float - can contain decimal values
+
+float_one = 220.8
+
+# Boolean - True/false
+status = False
+
+# None Type
+no_value = None
+print(type(no_value))
+
